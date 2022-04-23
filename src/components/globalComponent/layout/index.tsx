461641b1e9a14b9432578530acc306862f236819
Wilson Lo <wilsonlo@freehunter.hk>
2022-04-24 00:26:31 +0800

    feat(css): update hover effect

--- src/components/globalComponent/layout/index.tsx
+++ src/components/globalComponent/layout/index.tsx
@@ -7,7 +7,7 @@ import SnackBar from "../snackBar"
 const Layout = ({children}) => {
 
   return (
-    <div className="w-screen h-screen">
+    <div className="h-screen">
       <LoginDialog />
       <SnackBar />
       <div className="">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 // React 
 import React from "react";
 import { 
+  Avatar,
   Breadcrumbs,
   Divider,
   Link,
@@ -16,7 +17,7 @@ const Home = () => {
     <div className="flex flex-col">
       <div className="flex flex-col h-screen items-center justify-between">
         <p className="text-pink-400 text-base font-bold mt-8">An Engineer & A Photographer & An athlete</p>
-        <img className="w-96 h-84" src="/assets/icon.png"/>
+        <img className="w-96 h-84 transform-gpu hover:scale-110 transition duration-150 hover:ease-inn" src="/assets/icon.png"/>
         <div className="mb-8">
           <Breadcrumbs aria-label="breadcrumb">
             <Link href="/#about" underline="hover" className="text-base text-pink-400">ABOUT</Link>
@@ -26,45 +27,56 @@ const Home = () => {
         </div>
       </div>
       <div id="about" className="flex lg:flex-row flex-col">
-        <div className="md:w-1/2 w-full flex flex-col bg-pink-400 items-center justify-between">
+        <div className="lg:w-1/2 w-full flex flex-col bg-pink-400 items-center justify-between">
           <span></span>
           <img className="w-52 h-52" src="/assets/about.svg"/>
           <div className="mb-8 justify-self-end">
-            <Breadcrumbs className="text-white" aria-label="breadcrumb">
+            <Breadcrumbs className="lg:flex hidden text-white" aria-label="breadcrumb">
               <Link href="/#about" underline="hover" className="text-base text-white">ABOUT</Link>
               <Link href="/#work" underline="hover" className="text-base text-white">WORK</Link>
               <Link href="/#contact" underline="hover" className="text-base text-white">CONTACT</Link>
             </Breadcrumbs>
           </div>
         </div>
-        <div className="md:w-1/2 w-full flex flex-col bg-white items-center justify-center border">
+        <div className="lg:w-1/2 w-full flex flex-col bg-white items-center justify-center border">
           <div className="p-4 m-16 space-y-8">
-            <p className="font-bold leading-loose">I am a Wilson LO, an eningeer from Hong Kong 🀄︎</p>
+            <div className="flex hover:shadow-md hover:border hover:rounded p-4 space-x-4 items-center transition duration-150 hover:ease-in">
+              <Avatar sx={{ width: 72, height: 72 }} src="https://media-exp1.licdn.com/dms/image/C5103AQEDjEg-clE66A/profile-displayphoto-shrink_800_800/0/1552970127855?e=1655942400&v=beta&t=_44ZXmxQO8Tn_Km6olNtMplBjdS9sgv2Nn1dRL47y1k"/>
+              <div className="flex flex-col">
+                <p className="text-2xl font-bold leading-loose">Wilson LO</p>
+                <p className="text-xl leading-loose">To Infinity and Beyond!</p>
+              </div>
+            </div>
+            <div className="space-y-8 p-4 hover:shadow-md hover:border hover:rounded transition duration-150 hover:ease-in">
+              <p className="text-xl font-bold leading-loose">An Engineer 🧑‍🔧</p>
+              <p className="leading-loose">I am responsible for Technician Leader of Freehunter. Also, I am a freelancer since I was 18, these projects include full-stack, big data, machine learning, etc. so I am good at fast learning and broad knowledge area in software development with 6 years + of working experience.</p>
+            </div>
             <Divider />
-            <p className="text-xl font-bold leading-loose">An Engineer 🧑‍🔧</p>
-            <p className="leading-loose">I am responsible for Technician Leader of Freehunter. Also, I am a freelancer since I was 18, these projects include full-stack, big data, machine learning, etc. so I am good at fast learning and broad knowledge area in software development with 6 years + of working experience.</p>
+            <div className="space-y-8 p-4 hover:shadow-md hover:border hover:rounded transition duration-150 hover:ease-in">
+              <p className="text-xl font-bold leading-loose">A Photographer 📷</p>
+              <p className="leading-loose">I am a part-time photographer, focusing on portrait photography and fine-art contemporary landscape, and I especially love creating images with a "Japan" feel. Now I am using Fujifilm XE-4.</p>
+            </div>
             <Divider />
-            <p className="text-xl font-bold leading-loose">A Photographer 📷</p>
-            <p className="leading-loose">I am a part-time photographer, focusing on portrait photography and fine-art contemporary landscape, and I especially love creating images with a "Japan" feel. Now I am using Fujifilm XE-4.</p>
-            <Divider />
-            <p className="text-xl font-bold leading-loose">An athlete 🏹</p>
-            <p className="leading-loose">I am an hoilday athlete, I love sport including long run, swimming, and also I am a professional archery with compound bow, currently work hard to being a Asian Games representative of Hong Kong before 2026.</p>
+            <div className="space-y-8 p-4 hover:shadow-md hover:border hover:rounded transition duration-150 hover:ease-in">
+              <p className="text-xl font-bold leading-loose">An athlete 🏹</p>
+              <p className="leading-loose">I am an hoilday athlete, I love sport including long run, swimming, and also I am a professional archery with compound bow, currently work hard to being a Asian Games representative of Hong Kong before 2026.</p>
+            </div>
           </div>
         </div>
       </div>
       <div id="work" className="flex lg:flex-row flex-col">
-        <div className="md:w-1/2 w-full flex flex-col bg-white items-center border justify-between">
+        <div className="lg:w-1/2 w-full flex flex-col bg-white items-center border justify-between">
         <span></span>
           <img className="w-52 h-52" src="/assets/work.svg"/>
           <div className="mb-8 justify-self-end">
-            <Breadcrumbs className="text-black" aria-label="breadcrumb">
+            <Breadcrumbs className="lg:flex hidden text-black" aria-label="breadcrumb">
               <Link href="/#about" underline="hover" className="text-base text-black">ABOUT</Link>
               <Link href="/#work" underline="hover" className="text-base text-black">WORK</Link>
               <Link href="/#contact" underline="hover" className="text-base text-black">CONTACT</Link>
             </Breadcrumbs>
           </div>
         </div>
-        <div className="md:w-1/2 w-full flex flex-col bg-white items-center justify-center border">
+        <div className="lg:w-1/2 w-full flex flex-col bg-white items-center justify-center border">
           <div className="p-4 m-16 space-y-8">
             <p className="text-2xl font-bold leading-loose">Tech Stack</p>
             <p className="text-xl font-bold leading-loose">Frontend</p>
@@ -166,18 +178,18 @@ const Home = () => {
         </div>
       </div>
       <div id="contact" className="flex lg:flex-row flex-col h-screen">
-        <div className="md:w-1/2 w-full flex flex-col bg-black items-center justify-between">
+        <div className="lg:w-1/2 w-full flex flex-col bg-black items-center justify-between">
           <span></span>
           <img className="w-52 h-52" src="/assets/contact.svg"/>
           <div className="mb-8 justify-self-end">
-              <Breadcrumbs className="text-black" aria-label="breadcrumb">
+              <Breadcrumbs className="lg:flex hidden text-pink-400" aria-label="breadcrumb">
                 <Link href="/#about" underline="hover" className="text-base text-pink-400">ABOUT</Link>
                 <Link href="/#work" underline="hover" className="text-base text-pink-400">WORK</Link>
                 <Link href="/#contact" underline="hover" className="text-base text-pink-400">CONTACT</Link>
               </Breadcrumbs>
           </div>
         </div>
-        <div className="md:w-1/2 w-full flex flex-col bg-white items-center justify-center border">
+        <div className="lg:w-1/2 w-full flex flex-col bg-white items-center justify-center border">
           <div className="p-4 m-16 space-y-8">
             <p className="font-bold leading-loose">If you want to collaborate on crafting amazing experience for people. you are very welcome to contact me.</p>
             <p className="leading-loose">I am available for freelance projects and full-time employment.</p>
