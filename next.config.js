e14061686ae650b92f43424e9a850219d8a98102
Wilson Lo <wilsonlo@freehunter.hk>
2022-04-23 22:29:43 +0800

    fix(i18n):remove i18n

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
-const nextTranslate = require('next-translate')
-
-module.exports = nextTranslate({
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'https://api.json-generator.com/templates/:path*',
-                // destination: process.env.API,
-            }
-        ]
-    }
-});
+const nextConfig = {
+    /* config options here */
+  }
+  
+export default nextConfig
