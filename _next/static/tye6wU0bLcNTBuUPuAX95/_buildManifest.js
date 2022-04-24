a2c53323467689600bceef330615cac3d3727acc
WilsonLTL <31318783+WilsonLTL@users.noreply.github.com>
2022-04-24 12:04:03 +0000

    Deploying to gh-pages from  @ b5b6d94812b28523ff7e3c0bd35e70d177a65303 🚀

--- _next/static/tye6wU0bLcNTBuUPuAX95/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-68443ef7411c0fc9452c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-92b55874124a8ec545f3.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
